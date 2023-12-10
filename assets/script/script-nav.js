da0c81fa0553c5469b9b37ecefa3b58343dd8b50
LeleCastanheira <lelecastanheira@hotmail.com>
2023-12-10 19:59:07 -0300

    add responsividade na home

---
+++ assets/script/script-nav.js
@@ -0,0 +1,15 @@
+/* ----------------------SCROLL NAVABAR --------------------------------- */
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+  header.classList.toggle("sticky", window.scrollY >60)
+});
+
+/* -----------------------MENU ICON ------------------------------------- */
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('open');
+};
